81772c9a97841fd0ff1820893344e981905fa0ef
3schools <85482920+3schools@users.noreply.github.com>
2021-08-13 13:31:21 +0530

    Update changeN.js

--- changeN.js
+++ changeN.js
@@ -1,16 +1,11 @@
 window.addEventListener('load', function() {
   var status = document.getElementById("status");
-  var log = document.getElementById("log");
-
-  function updateOnlineStatus(event) {
-    var condition = navigator.onLine ? "online" : "offline";
-
-    status.className = condition;
-    status.innerHTML = condition.toUpperCase();
-
-    log.insertAdjacentHTML("beforeend", "Event: " + event.type + "; Status: " + condition);
+  window.addEventListener('online',  function(){
+  
+  alert("you are online");
+  });
+  function offlinea(){
+  alert("you are offline");
   }
-
-  window.addEventListener('online',  updateOnlineStatus);
-  window.addEventListener('offline', updateOnlineStatus);
+  window.addEventListener('offline', offlinea);
 });
