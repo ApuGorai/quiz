ff7b19e4d5890408aaedbade5bbafc25e5f71d03
3schools <85482920+3schools@users.noreply.github.com>
2021-08-19 14:59:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,37 +44,3 @@ function share() {
         location.href = 'https://api.whatsapp.com/send?text=' + encodeURIComponent(text + ' - ') + location.href
     }
 }   
-
-var loadFile = function(event) {
- var reader = new FileReader();
- reader.onload = function(){
- var output = document.getElementById('uImg');
- output.value = reader.result;
- };
- reader.readAsDataURL(event.target.files[0]);
- 
- };
-
-function addUser(){
- var uN = document.getElementById("uN").value;
- var uImg= document.getElementById("uImg").value;
- 
- localStorage.setItem("userName", uN);
- localStorage.setItem("userPic", uImg);
- 
- document.getElementById("uC").style.display="none";
- userPic();
- }
- 
- function userPic(){
-  var userName = localStorage.getItem("userName");
-  var userImg = localStorage.getItem("userPic");
-  if(userName != null){
-  document.getElementById("userN").innerHTML= userName;
-  document.getElementById("userImg").src= userImg;
-  document.getElementById("uC").style.display="none";
-  }
-  
-  }
-  
- userPic();
